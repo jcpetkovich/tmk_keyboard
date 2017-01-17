90cac7052f6aa9d9d02ade5e8f12dbfbbd009d5c
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2017-01-17 16:58:44 -0500

    Fix broken keyref.

--- keyboard/atreus/keymap_jc.c
+++ keyboard/atreus/keymap_jc.c
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [NUMS] = {
   {_______, _______, _______, KC_ASTR, KC_SLSH, _______, _______, KC_7,    KC_8,    KC_9,    KC_SLSH},
   {_______, _______, KC_EQL,  KC_PLUS, KC_MINS, _______, KC_LPRN, KC_4,    KC_5,    KC_6,    KC_ASTR},
-  {_______, _______, KC_DOT,  KC_COMM, KC_SCLN, _______, C_RPRN,  KC_1,    KC_2,    KC_3,    KC_MINS},
+  {_______, _______, KC_DOT,  KC_COMM, KC_SCLN, _______, KC_RPRN, KC_1,    KC_2,    KC_3,    KC_MINS},
   {_______, _______, _______, _______, _______, _______, _______, KC_0,    KC_DOT,  KC_EQL,  KC_PLUS}
 },
 [FKEY] = {
